5c225de6f821cd938c5cd8f72cb53982a996aa65
Zane Stearman <zstearman@mail.greenriver.edu>
2019-05-06 17:09:03 -0400

    Index tests/alterations

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 session_start();
 
 //Turn on error reporting
-ini_set('display_errors', 1);
+
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 //Required file
 require_once('vendor/autoload.php');
-require_once('model/validate.php');
 
 //Instantiate Fat-Free
 $f3 = Base::instance();
@@ -19,17 +19,21 @@ $f3->set('DEBUG', 3);
 
 
 //Define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
 
-    echo "<h1>My pets</h1>";
-    echo "<a href='order'>Place an Order</a>";
-});
+    //instantiate pet class
+    $pet1 = new Pet();
+    $f3->set('pet1', $pet1);
 
-//Define a summary route
-$f3->route('GET /summary', function() {
 
-    //Display summary
+    $pet2 = new Pet("Roofus");
+    $f3->set('pet2', $pet2);
+
+    $pet3 = new Pet("Charles", "gray");
+    $f3->set('pet3', $pet3);
+
     $view = new Template();
+    echo 'heyyy';
     echo $view->render('views/my-pets.html');
 });
 
